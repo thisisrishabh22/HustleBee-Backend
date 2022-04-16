4d5b4fbc1b72feefac7fb0cd6f8ad75fd8811333
Rishabh Singh <ris8645208@gmail.com>
2022-04-16 20:41:18 +0530

    feat: add cors

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
 from decouple import config
+from flask_cors import CORS, cross_origin
 
 # Prod Or Dev
 ENV = config("ENV")
@@ -34,29 +35,38 @@ user = app.db["users"]
 
 # JWT Config
 app.config["JWT_SECRET_KEY"] = "OizT0h_e6wDiIBlAX2s"
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
+@cross_origin()
 @app.route("/dashboard")
 @jwt_required
 def dasboard():
     return jsonify(msg="Welcome!")
 
-
+@cross_origin(origin='*')
 @app.route("/register", methods=["POST"])
 def register():
-    email = request.form["email"]
+    if request.is_json:
+        email = request.json["email"]
+        name = request.json["name"]
+        password = request.json["password"]
+    else:
+        email = request.form["email"]
+        name = request.form["name"]
+        password = request.form["password"]
+    
     resp_user = user.find_one({"email": email})
     if resp_user:
         return jsonify(msg="User Already Exist"), 409
     else:
-        name = request.form["name"]
-        password = request.form["password"]
         pw_hash = bcrypt.generate_password_hash(password)
         user_info = dict(name=name, email=email, password=pw_hash, token="")
         user.insert_one(user_info)
         return jsonify(msg="User added sucessfully"), 201
 
-
+@cross_origin(origin='*')
 @app.route("/login", methods=["POST"])
 def login():
     if request.is_json:
